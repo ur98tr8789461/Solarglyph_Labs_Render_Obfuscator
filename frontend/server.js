e3d95ee8b08e1a777b9638d6acf33535b11575a4
Noah <183996189+ur98tr8789461@users.noreply.github.com>
2026-09-07 20:28:04 +0100

    Log server version on startup
    Added version logging to server startup.

--- frontend/server.js
+++ frontend/server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const Version = "1.0.0";
 app.use(express.static("public"));
 // Serve static files
 app.use(express.static(path.join(__dirname, "public")));
-
+ 
+ 
 app.listen(3000, () => {
   console.log("Solarglyph Labs site running on port 3000");
 // Homepage
@@ -14,9 +15,8 @@ app.get("/", (req, res) => {
 });
 
 
-
-// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Solarglyph Labs site running on port ${PORT}`);
+  console.log(`Version: ${Version}`);
 });
